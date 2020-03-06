36d6a2a14a69526f90f464664c20f1aeb87ca958
Vishal.Thakur <vishal.thakur@tui.co.uk>
2020-03-06 00:39:27 +0000

    updating stages

--- infra/terraform/modules/airflow/lc.tf
+++ infra/terraform/modules/airflow/lc.tf
@@ -1,33 +1,9 @@
 
 
-
-    
-
-resource "aws_launch_configuration" "master_cng_lc" {
-  name_prefix          = "${var.environment}-airflow-master-lc-"
-  image_id             = "${data.aws_ami.master_cng_ami.id}"
-  instance_type        = "${var.lc_master_instance_type}"
-  key_name             = "${var.lc_key_name}"
-
-
-  root_block_device {
-    volume_type           = "${var.lc_root_vl_type}"
-    volume_size           = "${var.lc_root_vl_size}"
-    delete_on_termination = "true"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_instance" "example"{
+ami  = "ami-13be557e"
+instance_type="t2.micro"
 }
 
-data "aws_ami" "master_cng_ami" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["Amazon Linux 2 AMI (HVM), SSD Volume Type *"]
-  }
-
-}
 
 
